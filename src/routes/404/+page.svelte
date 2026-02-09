03efcea8dfb76a5db0dd6199c373168a7e334e47
Dreadz <vcs@dreadzverse.com>
2026-02-09 21:02:44 +0100

    Update 404 page

--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -1,6 +1,13 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+</script>
+
 <main class="grow">
 	<section class="flex h-full w-full flex-col items-center justify-center gap-4">
 		<h1 class="text-8xl font-bold">Dreadz</h1>
 		<p class="text-lg font-extralight">404 Not Found</p>
+		<a class="transition-all duration-300 hover:scale-110 hover:text-sky-600" href={resolve('/')}
+			>Home</a
+		>
 	</section>
 </main>
